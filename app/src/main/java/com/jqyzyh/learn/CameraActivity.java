ace357d216331d42d0d42aedc81f0ae9afeafd36
jqyzyh <jqyzyh@gmail.com>
2016-07-21 10:56:28 +0800

    commit yh

--- app/src/main/java/com/jqyzyh/learn/CameraActivity.java
+++ app/src/main/java/com/jqyzyh/learn/CameraActivity.java
@@ -5,9 +5,11 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaCodec;
 import android.media.MediaRecorder;
 import android.os.Build;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,14 +28,17 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
     private HolderCallback holderCallback;
     MediaRecorder mediaRecorder = new MediaRecorder();
 
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    SurfaceHolder surfaceHolder;
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
         SurfaceView sv = (SurfaceView) findViewById(R.id.sf_camera2);
-        sv.getHolder().addCallback(this);
+        surfaceHolder = sv.getHolder();
+        surfaceHolder.addCallback(this);
 
 //        surfaceView = (SurfaceView) findViewById(R.id.sf_camera);
 //        cameraHandler = new CameraHandler();
@@ -42,42 +47,44 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
 //        cameraHandler.openCamera(this);
 //        holderCallback.setCamera(cameraHandler);
 
-
-    }
+    MediaCodec mediaCodec = null;
+    mediaCodec.createInputSurface();
+}
 
     Paint paint = new Paint();
-
-    SurfaceHolder surfaceHolder;
 
     Runnable runnable =new Runnable() {
 
 
         @Override
         public void run() {
-            try {
-                Thread.sleep(50);
-                if(surfaceHolder!= null){
-                    Canvas canvas = surfaceHolder.lockCanvas(new Rect(0,0,1000,1000));
-                    if(canvas != null){
-                        Log.d("mylog", "mylog");
-                        paint.setColor(Color.WHITE);
-                        paint.setStyle(Paint.Style.FILL);
-                        canvas.drawARGB(255,255,255,255);
-                        surfaceHolder.unlockCanvasAndPost(canvas);
+            while(surfaceHolder != null){
+                try {
+                    Thread.sleep(50);
+                    if(surfaceHolder!= null){
+                        Canvas canvas = surfaceHolder.lockCanvas();
+                        if(canvas != null){
+                            Log.d("mylog", "mylog");
+                            paint.setColor(Color.WHITE);
+                            paint.setStyle(Paint.Style.FILL);
+                            canvas.drawARGB(255,0,0,0);
+                            canvas.drawARGB(255,255,255,255);
+                            surfaceHolder.unlockCanvasAndPost(canvas);
+                        }
                     }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
+
         }
     };
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
 //        cameraHandler.setSurface2(holder);
-        surfaceHolder = holder;
+//        surfaceHolder = holder;
         new Thread(runnable).start();
     }
 
@@ -88,6 +95,6 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-
+        surfaceHolder = null;
     }
 }
